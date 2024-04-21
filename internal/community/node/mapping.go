0355a71c00c67aa5a919a77f3264956654a8fe86
徐超越 <993921@qq.com>
2024-04-21 12:57:46 +0800

    WIP: member list implement

--- internal/community/node/mapping.go
+++ internal/community/node/mapping.go
@@ -2,6 +2,7 @@ package node
 
 import "another_node/internal/community/storage"
 
+// GetPublicKey get public key by hashed account
 func GetPublicKey(hashedAccount *string) string {
 	if member, err := storage.TryFindMember(*hashedAccount); err != nil {
 		return ""

04fe68b82ed31ae592bda27e8744e14df3b7d8c8
徐超越 <993921@qq.com>
2024-07-13 17:21:17 +0800

    cors

--- internal/web_server/middlewares/cors.go
+++ internal/web_server/middlewares/cors.go
@@ -8,8 +8,10 @@ import (
 
 func CorsHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // 设置允许的来源
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, X-Requested-With, User-Agent, Origin, Host, Connection, Accept, Accept-Encoding, Accept-Language, DNT, Keep-Alive, Referer, Sec-Fetch-Dest, Sec-Fetch-Mode, Sec-Fetch-Site")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)

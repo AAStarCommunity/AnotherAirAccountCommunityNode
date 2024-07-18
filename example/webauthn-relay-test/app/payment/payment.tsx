4c888a22a981dbda1a1e8f0e8083ffea6718fb84
徐超越 <993921@qq.com>
2024-07-18 22:33:54 +0800

    practice spc

--- example/webauthn-relay-test/app/payment/payment.tsx
+++ example/webauthn-relay-test/app/payment/payment.tsx
@@ -6,20 +6,20 @@ import { startAuthentication } from "@simplewebauthn/browser";
 import { PublicKeyCredentialRequestOptionsJSON } from "@simplewebauthn/types";
 
 export const PasskeyPayment = async (formData: FormData) => {
-  let amount = formData.get("amount") as string;
-  await generateAuthPasskeyPublicKey("ab@de.com", amount);
+  await isSecurePaymentConfirmationSupported();
+  let txdata = formData.get("txdata") as string;
+  await generateAuthPasskeyPublicKey(txdata);
 };
 
-const generateAuthPasskeyPublicKey = async (email: string, amount: string) => {
+const generateAuthPasskeyPublicKey = async (txdata: string) => {
   const origin = window.location.origin;
   const nonce = Math.floor(Math.random() * 100001).toString();
   const resp = await api.post(
     API.PASSKEY_PAYMENT,
     {
       origin,
-      amount,
       nonce,
-      txdata: "this_is_a_fake_tx_data",
+      txdata: txdata,
     },
     {
       headers: {
@@ -29,13 +29,17 @@ const generateAuthPasskeyPublicKey = async (email: string, amount: string) => {
   );
   const json = resp.data.data as PublicKeyCredentialRequestOptionsJSON;
   if (json !== null) {
+    /*
+    Practice the SPC flow
+    // const req = await requestSPC(json);
+    // await req.show();
+    // return;
+    */
     const attest = await startAuthentication(json);
     const verifyResp = await api.post(
       API.PASSKEY_PAYMENT_VERIFY +
         "?origin=" +
         encodeURIComponent(origin) +
-        "&email=" +
-        email +
         "&nonce=" +
         nonce,
       attest,
@@ -52,3 +56,105 @@ const generateAuthPasskeyPublicKey = async (email: string, amount: string) => {
     }
   }
 };
+
+const requestSPC = (
+  json: PublicKeyCredentialRequestOptionsJSON
+): PaymentRequest => {
+  const { challenge, allowCredentials } = json;
+  return new PaymentRequest(
+    [
+      {
+        // Specify `secure-payment-confirmation` as payment method.
+        supportedMethods: "secure-payment-confirmation",
+        data: {
+          // The RP ID
+          rpId: "rp.example",
+
+          // List of credential IDs obtained from the RP server.
+          credentialIds: allowCredentials!.map(
+            (credential) => new TextEncoder().encode(credential.id).buffer
+          ),
+
+          // The challenge is also obtained from the RP server.
+          challenge: new TextEncoder().encode(challenge),
+
+          // A display name and an icon that represent the payment instrument.
+          instrument: {
+            displayName: "Fancy Card ****1234",
+            icon: "https://www.aastar.xyz/_next/image?url=%2Flogo.png&w=828&q=75",
+            iconMustBeShown: false,
+          },
+
+          // The origin of the payee (merchant)
+          payeeOrigin: "https://localhost:3000",
+
+          // The number of milliseconds to timeout.
+          timeout: 360000, // 6 minutes
+        },
+      },
+    ],
+    {
+      // Payment details.
+      total: {
+        label: "Total",
+        amount: {
+          currency: "USD",
+          value: "5.00",
+        },
+      },
+    }
+  );
+};
+
+const isSecurePaymentConfirmationSupported = async () => {
+  if (!window.PaymentRequest) {
+    return [false, "Payment Request API is not supported"];
+  }
+
+  try {
+    // The data below is the minimum required to create the request and
+    // check if a payment can be made.
+    const supportedInstruments = [
+      {
+        supportedMethods: "secure-payment-confirmation",
+        data: {
+          // RP's hostname as its ID
+          rpId: "rp.example",
+          // A dummy credential ID
+          credentialIds: [new Uint8Array(1)],
+          // A dummy challenge
+          challenge: new Uint8Array(1),
+          instrument: {
+            // Non-empty display name string
+            displayName: " ",
+            // Transparent-black pixel.
+            icon: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+P+/HgAFhAJ/wlseKgAAAABJRU5ErkJggg==",
+          },
+          // A dummy merchant origin
+          payeeOrigin: "https://non-existent.example",
+        },
+      },
+    ];
+
+    const details = {
+      // Dummy shopping details
+      total: { label: "Total", amount: { currency: "USD", value: "0" } },
+    };
+
+    const request = new PaymentRequest(supportedInstruments, details);
+    const canMakePayment = await request.canMakePayment();
+    return [canMakePayment, canMakePayment ? "" : "SPC is not available"];
+  } catch (error) {
+    console.log(error);
+    return [false, error];
+  }
+};
+
+isSecurePaymentConfirmationSupported().then((result) => {
+  const [isSecurePaymentConfirmationSupported, reason] = result;
+  if (isSecurePaymentConfirmationSupported) {
+    // Display the payment button that invokes SPC.
+  } else {
+    // Fallback to the legacy authentication method.
+  }
+});

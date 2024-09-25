d8e10b8b72797ba476d6cbd144e6becb11c1a20d
徐超越 <993921@qq.com>
2024-09-25 20:18:14 +0800

    chore: code clean

--- plugins/passkey_relay_party/signature_verify_util.go
+++ plugins/passkey_relay_party/signature_verify_util.go
@@ -3,24 +3,35 @@ package plugin_passkey_relay_party
 import (
 	"crypto/sha256"
 	"encoding/base64"
+	"net/http"
 
 	"github.com/go-webauthn/webauthn/protocol"
 	"github.com/go-webauthn/webauthn/protocol/webauthncose"
 )
 
-func VerifySignature(data *protocol.ParsedCredentialAssertionData, publicKey *string, signature []byte) (bool, error) {
+func ExtractAssertionData(ctx *http.Request) (authData, clientDataJson protocol.URLEncodedBase64, signature []byte, err error) {
+	p, err := protocol.ParseCredentialRequestResponse(ctx)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	authData = p.Raw.AssertionResponse.AuthenticatorData
+	clientDataJson = p.Raw.AssertionResponse.ClientDataJSON
+	signature = p.Response.Signature
+	return authData, clientDataJson, signature, nil
+}
+
+func VerifySignature(authData, clientDataJson protocol.URLEncodedBase64, publicKey *string, signature []byte) (bool, error) {
 	pubKeyBytes, err := base64.RawURLEncoding.DecodeString(*publicKey)
 	if err != nil {
 		return false, err
 	}
-	authData := data.Raw.AssertionResponse.AuthenticatorData
 	pubKey, err := webauthncose.ParsePublicKey(pubKeyBytes)
 	if err != nil {
 		return false, err
 	}
-	sig := data.Response.Signature
-	clientDataHash := sha256.Sum256(data.Raw.AssertionResponse.ClientDataJSON)
+	clientDataHash := sha256.Sum256([]byte(clientDataJson))
 	// sigData = authData + sha256(clientData)
-	sigData := append(authData, clientDataHash[:]...)
-	return webauthncose.VerifySignature(pubKey, sigData, sig)
+	sigData := append([]byte(authData), clientDataHash[:]...)
+	return webauthncose.VerifySignature(pubKey, sigData, signature)
 }
